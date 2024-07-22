1887111de5512132020149d4a72313160de11605
alimombeni <ali.mombenni@gmail.com>
2024-07-22 15:03:17 +0330

    [ADD: create home properties component]

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,16 @@
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import InfoBox from "@/components/InfoBox";
+import HomeProperties from "@/components/HomeProperties";
 
 
 const homePage = () => {
     return(
         <>
 
-<Hero/>
+            <Hero/>
             <InfoBoxes/>
+            <HomeProperties/>
 
         </>
     )
